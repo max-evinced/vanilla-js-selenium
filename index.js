41fab09ce475b149c28101f0d5dfef697e17d883
Max Dobeck <max.dobeck@evinced.com>
2024-05-03 14:24:53 -0400

    ignoring newly generated .json report

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const { Builder, Browser } = require('selenium-webdriver')
 const { beforeEach, describe, afterEach, it } = require('mocha')
 const assert = require('node:assert')
-const { EvincedSDK } = require('@evinced/js-selenium-sdk/dist/ev-selenium-sdk.bundle.js')
+const { EvincedSDK, setCredentials } = require('@evinced/js-selenium-sdk/dist/ev-selenium-sdk.bundle.js')
 
 let driver = null
 describe("Evinced Travel", () => {
     beforeEach(async () => {
         driver = await new Builder().forBrowser(Browser.CHROME).build()
         const evincedService = await new EvincedSDK(driver)
+        await setCredentials({
+            serviceId: process.env.AUTH_SERVICE_ID,
+            secret: process.env.AUTH_SECRET,
+        })
         await driver.manage().setTimeouts({implicit: 1000});
         await driver.get("https://demo.evinced.com")
-        const evincedIssues = await evincedService.evAnalyze()
+        const issues = await evincedService.evAnalyze()
+        evincedService.evSaveFile(issues, 'json', 'evinced-report.json');
     })
 
     afterEach(async () => {
